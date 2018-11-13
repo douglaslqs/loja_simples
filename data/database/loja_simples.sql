655a6b34e7db113b1e67f0de70bf2c5f13932825
Douglas Santos <douglasrock15@hotmail.com>
2018-11-13 03:29:19 -0200

    master - atualizacao do arquivo .sql

--- data/database/loja_simples.sql
+++ data/database/loja_simples.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `db_loja_simples` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `db_loja_simples`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: db_loja_simples
@@ -274,4 +276,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-13  2:54:18
+-- Dump completed on 2018-11-13  3:28:06
